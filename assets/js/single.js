2988943bc8ee518f45ed0b7d9f6754394f1fdb07
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-01-25 18:59:17 -0500

    added warning message for repos with 30+ issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,4 @@
-
+var limitWarningEl = document.querySelector("#limit-warning");
 var issuesContainer = document.querySelector("#issues-container");
 
 var getRepoIssues = function(repo) {
@@ -8,6 +8,9 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+                if (response.headers.get("Link")) {
+                   displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -51,6 +54,18 @@ var displayIssues = function(issues) {
         // append to the entire container
         issuesContainer.appendChild(issueEl);
     }
+}
+
+var displayWarning = function(repo) {
+    // add text to the warning container
+    limitWarningEl.textContent = `To see more than 30 issues, please visit `
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "Repository Issues on GitHub.com";
+    linkEl.setAttribute("href", `https://github.com/${repo}/issues`);
+    linkEl.setAttribute("target", "_blank");
+
+    // append to the warning container
+    limitWarningEl.appendChild(linkEl);
 }
 
 getRepoIssues("facebook/react");
